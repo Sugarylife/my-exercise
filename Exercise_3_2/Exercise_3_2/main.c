f9df1715da2887e2ccbb6fc8a2d94d71c40c7acb
Sugarylife <953882438@qq.com>
2021-03-02 22:44:51 +0800

    结构体（1）

---
+++ Exercise_3_2/Exercise_3_2/main.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+/*
+struct Book
+{
+	char name[20];
+	char auther[20];
+	short price;
+
+}b2,b3,b4;
+int main()
+{
+	struct Book b1 = { "C语言程序设计","谭浩强",55 };
+	printf("%s %s %d\n",b1.name,b1.auther,b1.price);
+}
+*/
+
+
+/*
+typedef struct Book
+{
+	char name[20];
+	char auther[20];
+	short price;
+}Book;
+int main()
+{
+	Book b2 = { "C++程序设计","谭浩强",55 };
+	printf("%s %s %d\n",b2.name,b2.auther,b2.price);
+	return 0;
+}
+
+*/
