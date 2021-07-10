434e6899c8c0ee76a8e740ff3ce2c8371cde2c95
Sugarylife <953882438@qq.com>
2021-07-10 11:04:39 +0800

    数据存储的练习

---
+++ project_21_07_09/project_21_07_09/test.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+#include<string.h>
+/*
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d ",a,b,c);
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	char a = -128;
+	printf("%u\n",a);
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	char a = 128;
+	printf("%u\n",a);
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	int i = -20;
+	unsigned int j = 10;
+	printf("%d\n",i+j);
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	unsigned int i;
+	for (i = 9; i >= 0; i--)
+	{
+		printf("%u\n",i);
+		Sleep(1000);
+	}
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d",strlen(a));
+	return 0;
+}
+*/
+
+
+/*
+int main()
+{
+	unsigned char i = 0;
+	for (i = 0; i < 255; i++)
+	{
+		printf("hello world!\n");
+	}
+	//ËÀÑ­»·
+	return 0;
+}
+*/
+
